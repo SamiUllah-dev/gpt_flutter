fb2f7295a88b9ee38b1d91b8029c641adaccccf8
Sami <samikhankakar3@gmail.com>
2023-03-10 23:20:07 +0500

    updated the gpt to turbo 3.5

--- lib/services/ai_handler.dart
+++ lib/services/ai_handler.dart
@@ -2,17 +2,24 @@ import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 
 class AIHandler {
   final _openAI = OpenAI.instance.build(
-    token: '<your openAI API key>',
-    baseOption: HttpSetup(receiveTimeout: 20000),
+    token: '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',
+    baseOption: HttpSetup(
+      receiveTimeout: const Duration(seconds: 60),
+      connectTimeout: const Duration(seconds: 60),
+    ),
   );
 
   Future<String> getResponse(String message) async {
     try {
-      final request = CompleteText(prompt: message, model: kTranslateModelV3);
-      final response = await _openAI.onCompleteText(request: request);
+      final request = ChatCompleteText(messages: [
+        Map.of({"role": "user", "content": message})
+      ], maxToken: 200, model: kChatGptTurbo0301Model);
+
+      final response = await _openAI.onChatCompletion(request: request);
       if (response != null) {
-        return response.choices[0].text.trim();
+        return response.choices[0].message.content.trim();
       }
+
       return 'Some thing went wrong';
     } catch (e) {
       return 'Bad response';
